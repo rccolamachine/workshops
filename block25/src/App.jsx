2f75df3139e81352ea4dc0a88e6e7dad3d94b5d8
rccolamachine <Chapleski17@gmail.com>
2023-11-14 14:59:52 -0500

    adding block25 workshop

---
+++ block25/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+// Write your Color component here
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
+
+const App = () => {
+  const [selectedColor, setSelectedColor] = useState("orange");
+
+  return (
+    <div id="container">
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
+      </div>
+      <div id="colors-list">
+        <Color color="red" setSelectedColor={setSelectedColor} />
+        <Color color="orange" setSelectedColor={setSelectedColor} />
+        <Color color="yellow" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="violet" setSelectedColor={setSelectedColor} />
+        <Color color="black" setSelectedColor={setSelectedColor} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
