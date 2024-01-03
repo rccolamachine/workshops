8dd2408dbd1e6d07de0f3fedf2760aa5ea33c63d
rccolamachine <Chapleski17@gmail.com>
2024-01-02 20:44:44 -0500

    updating npm and completing workshop

--- block32/db/client.js
+++ block32/db/client.js
@@ -1,10 +1,15 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/gamestore';
+const connectionString =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:password@localhost:5432/gamestore";
 
 const client = new Client({
   connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 module.exports = client;
