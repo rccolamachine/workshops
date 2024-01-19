89baf6f6b0a929bdaaeee908a840cbde0f3ecbf9
rccolamachine <Chapleski17@gmail.com>
2024-01-18 20:07:02 -0500

    fitness-tracker submission

--- block33/api/index.js
+++ block33/api/index.js
@@ -1,64 +1,74 @@
-// api/index.js
 const express = require('express');
-const apiRouter = express.Router();
-
+const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { getUserById } = require('../db');
-const { JWT_SECRET } = process.env;
+const client = require('../db/client');
+const { JWT_SECRET = 'neverTell'} = process.env;
+
+// GET /api/health
+router.get('/health', async (req, res, next) => {
+  try {
+    const uptime = process.uptime();
+    const {rows: [dbConnection]} = await client.query('SELECT NOW();');
+    const currentTime = new Date();
+    const lastRestart = new Intl.DateTimeFormat('en', {timeStyle: 'long', dateStyle: 'long', timeZone: "America/Los_Angeles"}).format(currentTime - (uptime * 1000));
+    res.send({message: 'healthy', uptime, dbConnection, currentTime, lastRestart});
+  } catch (err) {
+    next(err);
+  }
+});
+
 
 // set `req.user` if possible
-apiRouter.use(async (req, res, next) => {
+router.use(async (req, res, next) => {
   const prefix = 'Bearer ';
   const auth = req.header('Authorization');
-
-  if (!auth) {
-    // nothing to see here
+  
+  if (!auth) { // nothing to see here
     next();
   } else if (auth.startsWith(prefix)) {
     const token = auth.slice(prefix.length);
-
+    
     try {
-      const { id } = jwt.verify(token, JWT_SECRET);
-
+      const parsedToken = jwt.verify(token, JWT_SECRET);
+      
+      const id = parsedToken && parsedToken.id
       if (id) {
         req.user = await getUserById(id);
         next();
-      } else {
-        next({
-          name: 'AuthorizationHeaderError',
-          message: 'Authorization token malformed',
-        });
       }
-    } catch ({ name, message }) {
-      next({ name, message });
+    } catch (error) {
+      next(error);
     }
   } else {
     next({
       name: 'AuthorizationHeaderError',
-      message: `Authorization token must start with ${prefix}`,
+      message: `Authorization token must start with ${ prefix }`
     });
   }
 });
 
-apiRouter.use((req, res, next) => {
+router.use((req, res, next) => {
   if (req.user) {
-    console.log('User is set:', req.user);
+    console.log("User is set:", req.user);
   }
-
   next();
 });
 
+// ROUTER: /api/users
 const usersRouter = require('./users');
-apiRouter.use('/users', usersRouter);
+router.use('/users', usersRouter);
 
-const postsRouter = require('./posts');
-apiRouter.use('/posts', postsRouter);
+// ROUTER: /api/activities
+const activitiesRouter = require('./activities');
+router.use('/activities', activitiesRouter);
 
-const tagsRouter = require('./tags');
-apiRouter.use('/tags', tagsRouter);
+// ROUTER: /api/routines
+const routinesRouter = require('./routines');
+router.use('/routines', routinesRouter);
 
-apiRouter.use((error, req, res, next) => {
-  res.send(error);
-});
+// ROUTER: /api/routine_activities
+const routineActivitiesRouter = require('./routineActivities');
+router.use('/routine_activities', routineActivitiesRouter);
 
-module.exports = apiRouter;
+module.exports = router;
